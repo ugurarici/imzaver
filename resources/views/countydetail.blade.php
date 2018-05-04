a3c879306a1bc6c4eed1a3513928ccbd5888e35d
Uğur Arıcı <m.ugurarici@gmail.com>
2018-05-05 02:57:17 +0300

    add margin-bottom to body - fix addthis gadget bloackage

--- resources/views/countydetail.blade.php
+++ resources/views/countydetail.blade.php
@@ -26,8 +26,8 @@
 				<div class="card-header">{{ $office->name }}</div>
 				<div class="card-body text-secondary">
 					<h5 class="card-title">{{ $office->address }}</h5>
-					<a href="tel:{{ $office->phone }}" class="btn btn-danger"><span class="oi oi-phone"></span> {{ $office->phone }}</a>
-					<a href="https://www.google.com.tr/maps/search/{{ $office->address }}" class="btn btn-secondary" target="_blank"><span class="oi oi-map-marker"></span> Haritada Ara</a>
+					<a href="tel:{{ $office->phone }}" class="btn btn-danger mb-1"><span class="oi oi-phone"></span> {{ $office->phone }}</a>
+					<a href="https://www.google.com.tr/maps/search/{{ $office->address }}" class="btn btn-secondary mb-1" target="_blank"><span class="oi oi-map-marker"></span> Haritada Ara</a>
 				</div>
 			</div>
 		</div>
